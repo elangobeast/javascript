c61e24ef983762255f6db19b43088152d8654f1b
elangobeast <elangonith1107@gmail.com>
2025-03-19 15:20:58 +0530

    initial

---
+++ 1_intro/intro.js
@@ -0,0 +1,63 @@
+console.log("I am external JS file");
+
+//!  HOW MANY WAYS WE CAN WRITE INTERNAL JAVASCRIPT
+        // 1.INTEERNAL JAVASCRIPT
+        // 2.EXTERNAL JAVASCRIPT
+//! output methods in javascript
+
+
+console.log("today is our first javascript class");
+
+document.writeln("hello how are you?");
+
+alert("hello how are you?")
+
+// ! How to declare variable
+     //! var, let , const
+
+//! How to declare variable
+
+// for that we need 3 keywords ==> 1.var 1. let 3. const
+
+var a // variable declaration
+
+a= 20 // variable initialization 
+
+console.log (a)
+
+var b = 30
+console.log(b)
+
+var a=100           //! in var re declaration is possible
+console.log(a)
+
+let subject= "javascript"
+console.log(subject)
+
+//let subject   //!  re declaration is not possible 
+
+subject = "java"
+console.log(subject)    //! re initialization is possible
+
+
+//! const
+//! it is used to make the value constant . we can not change the value
+//! here declaration and initialization are we have to do in the same line.
+const pi=3.14  // dont type 3.14 in next line
+
+console.log(pi)
+
+//! datatype in javascript
+//! 1. primitive data type 
+
+//!      i> Number
+//!      ii> boolean
+//!      iii> string 
+//!      iv> undefined 
+//!       v> null
+//!      vi> big int
+
+//!     2.non primitive data type
+
+//!     function,object,array 
+
